2bcf651e7ddfa33354053f56f321be14d26758d1
Shinkei <shinkeygentatsu@yahoo.com.mx>
2017-07-29 10:57:59 -0500

    add the reducer to the app and subscribe the component tothe store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { AppState } from './reducers'; 
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  counter: Observable<number>;
+
+  constructor(private store: Store<AppState>){
+    this.counter = store.select<number>((state: AppState) => { // me subscribo al store para que me avise del cambio de counter
+      return state.counter;
+    });
+  }
 }
--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,6 +1,5 @@
 import { Action } from '@ngrx/store';
 
-
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const RESET = 'RESET';
